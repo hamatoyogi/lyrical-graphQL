220a5d5bc2e86bf8c764d14a4c6f5d61dfd8d1ae
Yoav Ganbar <yoav.ganbar@theculturetrip.com>
2019-11-25 09:42:22 +0200

    Lyrics create, lyric list, song detail

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -16,7 +16,7 @@ class SongList extends Component {
     return data.songs.map(({id, title}) => {
       return (
         <li key={id} className={"collection-item"}>
-          {title}
+          <Link to={ `/songs/${id}` }>{title}</Link>
           <i className={"material-icons right"} onClick={() => this.onSongDelete(id)}>delete</i>
         </li>
       )
